30334d94719d40719c3b11c9da4f1de3f2308b8d
berilkaragol <beril.karagol@ozu.edu.tr>
2022-01-29 13:25:51 +0300

    2. soru çözüldü.

---
+++ Q2.java
@@ -0,0 +1,21 @@
+
+public class Q2 {
+public static void main (String[] args){		
+int i = 0;
+int number = 0;
+String  primeNumbers = "";
+for (i = 350; i <= 2570; i++){ 		  	  
+int counter=0; 	  
+for(number =i; number>=1; number--){
+if(i%number==0){
+counter = counter + 1;
+ }
+}
+ if (counter ==2){
+primeNumbers = primeNumbers + i + " ";
+		  }	
+	       }	
+ System.out.println("Prime numbers from 1 to 100 are :");
+System.out.println(primeNumbers);
+	   }
+}
