9668f53c338d474eec0e068a70ae655f84451757
berilkaragol <beril.karagol@ozu.edu.tr>
2022-01-29 13:16:02 +0300

    1. soru çözüldü.

---
+++ HwStudies.java
@@ -0,0 +1,13 @@
+
+public class HwStudies {
+public static void main(String[] args) {
+	
+	    int fact = 1;
+	    for (int i = 1; i <= 4; i++) {
+	        fact = fact * i;
+	    }
+	    System.out.println("4! = "+ fact);
+	
+	
+}
+}
